c82f603e1da3dcb7675ac766d5a06f47e82bc6b5
afefelov <reg@afefelov.ru>
2012-08-19 17:45:54 +0600

    wsgi file

--- aurora/aurora/wsgi.py
+++ aurora/aurora/wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path = ['/web/aurora.local', '/web/aurora.local/releases/current/aurora.local', '/web/aurora.local/lib/python2.6/site-packages'] + sys.path
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'aurora.settings'
 os.environ['PYTHON_EGG_CACHE'] = '/web/aurora.local/.python-eggs'
 
 import django.core.handlers.wsgi
