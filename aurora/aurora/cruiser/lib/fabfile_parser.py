41b50b98adf082741fe81ec2751c3dc1b72c7937
Eugene Akentyev <eakentev@naumen.ru>
2012-08-20 12:09:22 +0600

    Some tests for parser

--- aurora/aurora/cruiser/lib/fabfile_parser.py
+++ aurora/aurora/cruiser/lib/fabfile_parser.py
@@ -1,14 +1,14 @@
 def get_source(code):
-    import_block = ""
+    import_block = []
     tasks = []
 
     for line in code:
         if 'import' in line:
-            import_block += line
+            import_block.append(line)
 
     parts = ''.join(code).split('def')[1:]
     for part in parts:
         def_name = part.split('(')[0].replace(' ', '')
         tasks.append({'name': def_name, 'body': "def" + part})
 
-    return import_block, tasks
+    return '\n'.join(import_block), tasks
--- aurora/aurora/cruiser/tests.py
+++ aurora/aurora/cruiser/tests.py
@@ -1,16 +1,30 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from lib.fabfile_parser import get_source
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class FabFileParsing(TestCase):
+    def test_import(self):
+        code = """import fabfile
+from fabric.api import run, local"""
+        lines = code.split("\n")
+        actual = get_source(lines)
+        expected = ("import fabfile\nfrom fabric.api import run, local", [])
+        self.assertEqual(expected, actual)
+
+    def test_def(self):
+        code = """def test(n1, n2):
+    return n1 + n2"""
+        lines = code.split("\n")
+        actual = get_source(lines)
+        expected = ("", [{'body': "def test(n1, n2):    return n1 + n2", 'name': "test"}])
+        self.assertEqual(expected, actual)
+
+    def test_def_and_import(self):
+        code = """from fabric.api import local
+def test(n1, n2):
+    local('echo test')
+    return n1 + n2"""
+        lines = code.split("\n")
+        actual = get_source(lines)
+        expected = ("from fabric.api import local", [{'body': "def test(n1, n2):    local('echo test')    return n1 + n2", 'name': "test"}])
+        self.assertEqual(expected, actual)
