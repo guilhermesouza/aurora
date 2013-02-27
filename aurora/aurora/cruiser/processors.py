7f1856ff71764cc89ab9aec8cf9fd59d9729b85f
Eugene Akentyev <ak3ntev@gmail.com>
2013-02-27 12:30:07 +0600

    Delete duplicate folder.

--- aurora/aurora/cruiser/processors.py
+++
@@ -1,7 +0,0 @@
-from models import Project, Task, Stage
-
-
-def sidebar_data(request):
-    return {'all_projects': Project.objects.order_by('name',),
-            'all_tasks': Task.objects.order_by('name',),
-            'all_stages': Stage.objects.order_by('name',)}
