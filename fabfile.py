1572880c9e00107ee72ca1c9f239ec879a2cf630
Eugene Akentyev <ak3ntev@gmail.com>
2013-04-01 13:57:40 +0600

    Remove codemirror.

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,10 @@ def local_setup():
         'python manage.py syncdb',
         'python manage.py migrate',
         'touch aurora/local_settings.py',
-        'echo "DEBUG = True" > aurora/local_settings.py'
+        'echo "DEBUG = True" > aurora/local_settings.py',
+        # Download codemirror to static folder
+        'wget https://raw.github.com/marijnh/CodeMirror/master/lib/codemirror.js -P static/codemirror',
+        'wget https://raw.github.com/marijnh/CodeMirror/master/lib/codemirror.css -P static/codemirror',
+        'wget https://raw.github.com/marijnh/CodeMirror/master/mode/python/python.js -P static/codemirror'
     ]
     map(local, commands)
