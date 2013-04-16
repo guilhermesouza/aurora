e5c4f60fe578f7f7bcd54f4154e95daa1487c47f
Eugene Akentyev <ak3ntev@gmail.com>
2013-04-16 21:39:04 +0600

    Delet.

--- aurora/cruiser/templatetags/recent_deployments_tag.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-def show_recent_deployments(deps):
-    return {'deps': deps}
-
-register.inclusion_tag('recent_deployments.html')(show_recent_deployments)
--- aurora/cruiser/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
-
-
-def validate_presence(value):
-    """Validate presence of value"""
-    if any([value is None, str(value).strip() == '']):
-        raise ValidationError(_('Value is blank.'))
