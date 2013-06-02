5f5760863e80ee8300bb42dc966c0b5b4ecdc70f
Eugene Akentyev <ak3ntev@gmail.com>
2013-06-02 06:37:49 +0000

    Simple ajax notifications.

--- aurora_app/context_processors.py
+++ aurora_app/context_processors.py
@@ -1,24 +1,8 @@
-from flask import g
-
 from aurora_app import app
-from aurora_app.database import db
-from aurora_app.models import Project, Notification
+from aurora_app.models import Project
 
 
 @app.context_processor
 def projects():
     """Returns all projects."""
     return {'projects': Project.query.all()}
-
-
-@app.context_processor
-def notifications():
-    """Marks notifications as seen and returns to user."""
-    notifications = Notification.query.filter_by(seen=False, user=g.user) \
-                                      .order_by('created_at').all()
-    # Update notifications
-    for notification in notifications:
-        notification.seen = True
-    db.session.commit()
-
-    return {'notifications': notifications}
--- aurora_app/views/notifications.py
+++ aurora_app/views/notifications.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g, json
 
+from aurora_app.database import db
 from aurora_app.models import Notification
 
 mod = Blueprint('notifications', __name__, url_prefix='/notifications')
@@ -8,6 +9,22 @@ mod = Blueprint('notifications', __name__, url_prefix='/notifications')
 @mod.route('/table')
 def table():
     notifications = Notification.query.order_by("created_at desc").all()
-    # Using notifications_items because of notification's context processor.
     return render_template('notifications/table.html',
-                           notifications_items=notifications)
+                           notifications=notifications)
+
+
+@mod.route('/unseen')
+def unseen():
+    """Returns unseen notifications and updates them as seen."""
+    notifications = Notification.query.filter_by(seen=False, user=g.user) \
+                                      .order_by('created_at').all()
+
+    notifications_for_response = []
+    # Update notifications
+    for notification in notifications:
+        notification.seen = True
+        notifications_for_response.append({'message': notification.message,
+                                           'category': notification.category})
+    db.session.commit()
+
+    return json.dumps(notifications_for_response)
