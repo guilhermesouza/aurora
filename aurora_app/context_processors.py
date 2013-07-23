c3ce9ce9d83f27252309825b9abb97d3724a06b2
Eugene Akentyev <ak3ntev@gmail.com>
2013-07-23 22:12:43 +0600

    Move context processors.

--- aurora_app/context_processors.py
+++ aurora_app/context_processors.py
@@ -1,3 +1,7 @@
+import os
+
+from flask import url_for
+
 from aurora_app import app
 from aurora_app.models import Project
 
@@ -6,3 +10,18 @@ from aurora_app.models import Project
 def projects():
     """Returns all projects."""
     return {'projects': Project.query.all()}
+
+
+# To exclude caching of static
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
