2dbda5a8b2c1a0fdc1532c9812df09889fa0ee57
Eugene Akentyev <ak3ntev@gmail.com>
2013-08-01 15:05:31 +0600

    First experiment with submodules.

--- manage.py
+++ manage.py
@@ -12,6 +12,27 @@ manager.add_command("migrate", ManageMigrations())
 
 
 @manager.command
+def setup_submodules():
+    """Clones git submodules"""
+    import os
+
+    os.system('git submodule init')
+    os.system('git submodule update')
+
+    submodules_tags = {
+        'bootstrap': 'v3.0.0-rc1',
+        'bootbox': 'v3.3.0',
+        'select2': '3.4.1'
+    }
+
+    submodules_path_prefix = 'aurora_app/static/'
+
+    for submodule_name, tag in submodules_tags.iteritems():
+        os.system('cd {0}; git checkout {1}'.format(
+            os.path.join(submodules_path_prefix, submodule_name), tag))
+
+
+@manager.command
 def init_db():
     """Creates aurora database."""
     from aurora_app.constants import ROLES
